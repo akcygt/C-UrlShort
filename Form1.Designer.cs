f94a99778c466e2a7600c356b15d1fce78d96c98
Abdullah Koçyiğit <apdul.kocyigit@gmail.com>
2023-01-04 14:49:35 +0300

    C#UrlShortener

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(274, 76);
+            this.textBox1.MaximumSize = new System.Drawing.Size(280, 25);
+            this.textBox1.MinimumSize = new System.Drawing.Size(280, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(346, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kısalt!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(274, 118);
+            this.textBox2.MaximumSize = new System.Drawing.Size(280, 25);
+            this.textBox2.MinimumSize = new System.Drawing.Size(280, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(280, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(578, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Kopyala";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(578, 287);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Kopyala";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(422, 246);
+            this.textBox3.MaximumSize = new System.Drawing.Size(130, 25);
+            this.textBox3.MinimumSize = new System.Drawing.Size(130, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(274, 287);
+            this.textBox4.MaximumSize = new System.Drawing.Size(280, 25);
+            this.textBox4.MinimumSize = new System.Drawing.Size(280, 25);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(280, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(346, 318);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 35);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Değiştir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Mikado Bold DEMO", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 76);
+            this.label1.MaximumSize = new System.Drawing.Size(117, 40);
+            this.label1.MinimumSize = new System.Drawing.Size(117, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 40);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Uzun Linki Girin";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Mikado Bold DEMO", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(151, 120);
+            this.label2.MaximumSize = new System.Drawing.Size(117, 40);
+            this.label2.MinimumSize = new System.Drawing.Size(117, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 40);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Kısaltılmış Link";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Mikado Bold DEMO", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1, 246);
+            this.label3.MaximumSize = new System.Drawing.Size(267, 40);
+            this.label3.MinimumSize = new System.Drawing.Size(267, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(267, 40);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Değiştirilmesini istediğiniz Linki Girin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Mikado Bold DEMO", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1, 289);
+            this.label4.MaximumSize = new System.Drawing.Size(267, 40);
+            this.label4.MinimumSize = new System.Drawing.Size(267, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(267, 40);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Değiştirmek İstediğiniz Değeri Girin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Panton Rust Black Thin Base", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(323, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Link Kısaltıcı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Panton Rust Black Thin Base", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(289, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(254, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "KISA LINK GUNCELLEYİCİ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linkLabel1.Location = new System.Drawing.Point(274, 248);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(142, 16);
+            this.linkLabel1.TabIndex = 14;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://fkdk.ml/a.php?a=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void redirect(string a)
+        {
+            try
+            {
+                // Veritabanına bağlantı kurun
+                string connectionString = "server=your_server;user=your_user;database=your_database;password=your_password";
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+                // Kısaltılmış linki kullanarak veritabanından orijinal linki alın
+                string selectQuery = "SELECT original_link FROM links WHERE s = @a";
+                MySqlCommand command = new MySqlCommand(selectQuery, connection);
+                command.Parameters.AddWithValue("@a", a);
+                MySqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                string originalLink = reader["original_link"].ToString();
+
+                // Tarayıcıda orijinal linki açın
+                System.Diagnostics.Process.Start(originalLink);
+            }
+            catch (MySqlException ex)
+            {
+                // Veritabanına bağlantı kurulamadı veya veritabanından veri okunurken hata oluştu
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentNullException ex)
+            {
+                // System.Diagnostics.Process.Start() metoduna null değerleri geçirildi
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Veritabanına bağlantı kurun
+                string connectionString = "server=your_server;user=your_user;database=your_database;password=your_password";
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+                // Orijinal linki alın ve veritabanına yeni bir kayıt ekleyin
+                string originalLink = textBox1.Text;
+                string insertQuery = "INSERT INTO links (original_link) VALUES (@originalLink)";
+                MySqlCommand command = new MySqlCommand(insertQuery, connection);
+                command.Parameters.AddWithValue("@originalLink", originalLink);
+                command.ExecuteNonQuery();
+                // Yeni kaydın ID'sini alın ve kısaltılmış link oluşturun
+                long id = command.LastInsertedId;
+                string a = Convert.ToBase64String(BitConverter.GetBytes(id));
+
+                // Kısaltılmış linki veritabanına ekleyin
+                string updateQuery = "UPDATE links SET s = @a WHERE id = @id";
+                command = new MySqlCommand(updateQuery, connection);
+                command.Parameters.AddWithValue("@a", a);
+                command.Parameters.AddWithValue("@id", id);
+                command.ExecuteNonQuery();
+
+                // Kısaltılmış linki gösterin ve kopyalaması için bir button ekleyin
+                textBox2.Text = "http://fkdk.ml/a.php?a=" + a;
+                button2.Visible = true;
+
+
+
+
+
+
+
+
+            }
+            catch (MySqlException ex)
+            {
+                // Veritabanına bağlantı kurulamadı veya veritabanına kayıt eklenirken hata oluştu
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentNullException ex)
+            {
+                // Convert.ToBase64String() veya Clipboard.SetText() metodlarına null değerleri geçirildi
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Kısaltılmış linki kopyalayın
+                Clipboard.SetText(textBox2.Text);
+            }
+            catch (ArgumentNullException ex)
+            {
+                // Clipboard.SetText() metoduna null değerleri geçirildi
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Kısaltılmış linki kopyalayın
+                Clipboard.SetText(textBox4.Text);
+            }
+            catch (ArgumentNullException ex)
+            {
+                // Clipboard.SetText() metoduna null değerleri geçirildi
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            // Veritabanına bağlantı kurun
+            string connectionString = "server=your_server;user=your_user;database=your_database;password=your_password";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+
+            // Veritabanındaki s değerini alıp original_link değerini güncelleyin
+            string s = textBox3.Text;
+         // Uri uri = new Uri(s);
+         // string query = uri.Query;
+         // query = query.Replace("http://fkdk.ml/a.php?a=", "");
+            string newOriginalLink = textBox4.Text;
+            string updateQuery = "UPDATE links SET s = @newOriginalLink WHERE s = @s";
+            MySqlCommand command = new MySqlCommand(updateQuery, connection);
+            command.Parameters.AddWithValue("@newOriginalLink", newOriginalLink);
+            command.Parameters.AddWithValue("@s", s);
+            command.ExecuteNonQuery();
+            textBox4.Text = "http://fkdk.ml/a.php?a=" + textBox4.Text;
+            button4.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
